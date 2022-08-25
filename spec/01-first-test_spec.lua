7467fa381a7e1ac7c568739b69f601e6d81c4aa0
Thijs Schreijer <thijs@thijsschreijer.nl>
2022-08-25 16:51:19 +0200

    Initial commit

---
+++ spec/01-first-test_spec.lua
@@ -0,0 +1,9 @@
+describe("description", function()
+
+  local mymod = require "[module-name]"
+
+  it("replace this file with your own tests", function()
+    mymod.do_something("tell a lie!", true)
+  end)
+
+end)
